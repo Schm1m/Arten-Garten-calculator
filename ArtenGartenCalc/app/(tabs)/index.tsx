9fc9015e1f6ad3f30d0dbb1e5d887c6c0f3dbfa5
schm1m <tim@schm1m.com>
2025-11-25 23:38:51 +0100

    idk first layout or something

---
+++ ArtenGartenCalc/app/(tabs)/index.tsx
@@ -0,0 +1,31 @@
+import { StyleSheet } from 'react-native';
+
+import { Text, View } from '@/components/Themed';
+import { EditScreenOne } from '@/components/EditScreenInfo';
+
+export default function TabOneScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Automatic mode</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <EditScreenOne />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
